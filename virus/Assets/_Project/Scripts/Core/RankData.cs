9ca5357f028052e13318d2a45a952389f2c9d1b5
maengdori <2025233@sungil-i.kr>
2026-05-24 21:22:13 +0900

    [2026.05.24] ssh_턴 로직 추가

---
+++ virus/Assets/_Project/Scripts/Core/RankData.cs
@@ -0,0 +1,10 @@
+// 랭킹 기록
+[System.Serializable]
+public class RankData
+{
+    // 플레이어 이름
+    public string playerName;
+
+    // 클리어 일수
+    public int clearDay;
+}
---
+++ virus/Assets/_Project/Scripts/TimeSystem/TimeManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// 시간 관리
+public class TimeManager : MonoBehaviour
+{
+    public TimeData timeData;
+
+    // 타임 턴 1 소모
+    public void SpendTimeTurn()
+    {
+        timeData.timeTurn--;
+
+        if (timeData.timeTurn <= 0)
+        {
+            EndDay();
+        }
+    }
+
+    // 하루 종료
+    private void EndDay()
+    {
+        timeData.timeTurn = timeData.maxTimeTurn;
+        timeData.dayTurn--;
+    }
+}
