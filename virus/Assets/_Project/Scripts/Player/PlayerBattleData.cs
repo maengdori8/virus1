da91fabcbb5049f535e1ebd8e1e459fb7549f49b
maengdori <2025233@sungil-i.kr>
2026-05-25 00:23:35 +0900

    [2026.05.25] ssh_배틂 매니저 추가

---
+++ virus/Assets/_Project/Scripts/Player/PlayerBattleData.cs
@@ -0,0 +1,13 @@
+// 플레이어 전투 데이터
+[System.Serializable]
+public class PlayerBattleData
+{
+    // 공격력
+    public int attack;
+
+    // 방어력
+    public int defense;
+
+    // 오행 속성
+    public ElementType element;
+}
