94d48be70500b80e001981de16331d6002cb4524
maengdori <2025233@sungil-i.kr>
2026-05-24 23:54:28 +0900

    [2026.05.24] ssh_게임 매니저 최소 구조 추가

---
+++ virus/Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+// 게임 루프 관리
+public class GameManager : MonoBehaviour
+{
+    public TimeManager timeManager;
+    public SupplyManager supplyManager;
+    public RewardManager rewardManager;
+
+    public GameState gameState;
+
+    // 하루 시작
+    public void StartDay()
+    {
+        supplyManager.ConsumeDaily();
+        HealDaily();
+
+        if (gameState.hp.current <= 0)
+        {
+            GameOver();
+            return;
+        }
+
+        if (gameState.vaccineProgress >= 100)
+        {
+            GameClear();
+            return;
+        }
+    }
+
+    // 체력 자연 회복
+    private void HealDaily()
+    {
+        gameState.hp.current += gameState.hp.dailyHeal;
+
+        if (gameState.hp.current > gameState.hp.max)
+        {
+            gameState.hp.current = gameState.hp.max;
+        }
+    }
+
+    // 게임오버
+    private void GameOver()
+    {
+        Debug.Log("게임오버");
+        // 임시로 출력
+    }
+
+    // 클리어
+    private void GameClear()
+    {
+        Debug.Log("백신 완성");
+        // 임시로 출력
+    }
+}
