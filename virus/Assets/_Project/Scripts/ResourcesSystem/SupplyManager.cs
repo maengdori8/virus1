21ca2a889b80cf8014d0ac170238d06ffa07b42c
maengdori <2025233@sungil-i.kr>
2026-05-24 22:42:49 +0900

    [2026.05.24] ssh_물자,보상 로직 추가

---
+++ virus/Assets/_Project/Scripts/ResourcesSystem/SupplyManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+// 물자 관리
+public class SupplyManager : MonoBehaviour
+{
+    public SupplyData supplyData;
+    public HpData hpData;
+
+    // 하루 시작 시 호출
+    public void ConsumeDaily()
+    {
+        supplyData.current -= supplyData.dailyCost;
+
+        // 물자 부족하면 체력 깎기
+        if (supplyData.current < 0)
+        {
+            hpData.current += supplyData.current;
+            supplyData.current = 0;
+        }
+    }
+}
