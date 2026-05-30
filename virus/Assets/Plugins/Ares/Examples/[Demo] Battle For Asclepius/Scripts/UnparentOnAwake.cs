db358187dda7441882b334e7f1167fa5890cc31c
maengdori <2025233@sungil-i.kr>
2026-05-30 21:08:50 +0900

    [2026.05.30] ssh_plugin delete

--- virus/Assets/Plugins/Ares/Examples/[Demo] Battle For Asclepius/Scripts/UnparentOnAwake.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Ares.Examples {
-	public class UnparentOnAwake : MonoBehaviour {
-		void Awake(){
-			transform.SetParent(null, true);
-		}
-	}
-}
--- virus/Assets/Plugins/Ares/Scripts/Interactors/ItemAction.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-namespace Ares {
-	[System.Serializable]
-	public class ItemAction : AbilityAction {}
-}
