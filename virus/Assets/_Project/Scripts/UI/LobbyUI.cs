36658e09cd7742ef4b0cc4970af53b0a622242e9
maengdori <2025233@sungil-i.kr>
2026-08-09 20:30:00 +0900

    [2026.08.09] ssh_로비 UI 한글 주석 인코딩 수정

--- virus/Assets/_Project/Scripts/UI/LobbyUI.cs
+++ virus/Assets/_Project/Scripts/UI/LobbyUI.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class LobbyUI : MonoBehaviour
 {
-    [Header("�г�")]
+    [Header("패널")]
     public GameObject rankPanel;
 
     public void OnClickStart()
